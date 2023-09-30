3f8cbe1c8f6331ac54e925111420610c15490bc3
Elizabeth <tjeudy.e@gmail.com>
2023-09-30 11:13:40 -0400

    import firebase

---
+++ app.js
@@ -0,0 +1,75 @@
+/* FIREBASE  */
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+	apiKey: "AIzaSyBqtW-Y__Jo7INCBc06DNsHPMLN1z07rpM",
+	authDomain: "feelings-eb0c3.firebaseapp.com",
+	projectId: "feelings-eb0c3",
+	storageBucket: "feelings-eb0c3.appspot.com",
+	messagingSenderId: "791006282814",
+	appId: "1:791006282814:web:6753435b514bde2db23d6f",
+};
+const app = initializeApp(firebaseConfig);
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view");
+const viewLoggedIn = document.getElementById("logged-in-view");
+
+const signInWithGoogleButtonEl = document.getElementById(
+	"sign-in-with-google-btn"
+);
+
+const emailInputEl = document.getElementById("email-input");
+const passwordInputEl = document.getElementById("password-input");
+
+const signInButtonEl = document.getElementById("sign-in-btn");
+const createAccountButtonEl = document.getElementById("create-account-btn");
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
+
+signInButtonEl.addEventListener("click", authSignInWithEmail);
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
+
+/* === Main Code === */
+
+showLoggedOutView();
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+	console.log("Sign in with Google");
+}
+
+function authSignInWithEmail() {
+	console.log("Sign in with email and password");
+}
+
+function authCreateAccountWithEmail() {
+	console.log("Sign up with email and password");
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+	hideElement(viewLoggedIn);
+	showElement(viewLoggedOut);
+}
+
+function showLoggedInView() {
+	hideElement(viewLoggedOut);
+	showElement(viewLoggedIn);
+}
+
+function showElement(element) {
+	element.style.display = "flex";
+}
+
+function hideElement(element) {
+	element.style.display = "none";
+}
